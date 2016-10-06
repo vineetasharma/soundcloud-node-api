05a03ae4ad2a96809e135525837f6b43936089c6
Vineeta Sharma <vineetasharma18791@gmail.com>
2016-10-06 12:04:31 +0530

    playlist api added

--- helpers/enums.js
+++ helpers/enums.js
@@ -1,6 +1,7 @@
 module.exports = {
     BASE_URL: {
         USERS: 'http://api.soundcloud.com/users',
-        TRACKS: 'http://api.soundcloud.com/tracks'
+        TRACKS: 'http://api.soundcloud.com/tracks',
+        PLAYLISTS: 'http://api.soundcloud.com/playlists'
     }
 };
